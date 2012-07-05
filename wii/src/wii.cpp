afbcc30a5cdea51ede2302377dadf717762da2f7
Joakim Gebart <joakim.gebart@jge.se>
2012-07-05 09:13:11 +0200

    wii example: Modified to use Scope class.

--- wii/src/wii.cpp
+++ wii/src/wii.cpp
@@ -11,6 +11,7 @@ extern "C" {
 //~ #include <cstring>
 //~ #include <cstdio>
 //~ #include "stubs/newdel.hpp"
+#include "Framebuffer/Scope.hpp"
 
 #define USE_NUNCHUK true ///< Whether a nunchuk is connected
 
@@ -19,12 +20,19 @@ WiiMotionPlus wmp(CONF_SYSTEM_TWI, WiiMotionPlus::kDefaultMotionPlusPort, WiiMot
 
 void loop()
 {
+    static Scope wiiplot(*fb, 0, 0, 128, 64);
+    fb->clear();
     wmp.measure(); // Accelerometer and gyro are updated in lockstep
     wmp.measure();
-    gout << "A: " << wmp.getAccelerometerX() << ", " << wmp.getAccelerometerY() << ", " << wmp.getAccelerometerZ() << "\n";
+    wiiplot.put(wmp.getGyroY() * wmp.getGyroScaleY());
+    //~ gout.setPos(0, 0);
+    //~ gout << "A: " << wmp.getAccelerometerX() << ", " << wmp.getAccelerometerY() << ", " << wmp.getAccelerometerZ() << "\n";
     //~ fb->flush();
-    gout << "W: " << wmp.getGyroX() << ", " << wmp.getGyroY() << ", " << wmp.getGyroZ() << "\n";
+    //~ gout << "W: " << wmp.getGyroX() << ", " << wmp.getGyroY() << ", " << wmp.getGyroZ() << "\n";
+    //~ gout << "min_y: " << static_cast<uint32_t>(wiiplot.min_y) << " max_y: " << static_cast<uint32_t>(wiiplot.max_y) << "\n";
+    //~ gout << "scale_y: " << static_cast<uint32_t>(wiiplot.scale_y * 1000000.f) << "\n";
     fb->flush();
+    //~ for (;;);
 
     //~ for(int u = 0; u < 1000000; ++u)
     //~ {
@@ -46,6 +54,7 @@ int main(int, char**)
     //~ fb->clear(0xaa);
     //~ fb->line(0, 10, 100, 63, 0);
     //~ fb->flush();
+    fb->clear();
     for (;;) // never exit
     {
         loop();
